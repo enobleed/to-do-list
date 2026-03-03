665c7216fd138f66251f4a6fee68a2fca2b6f4d9
Adis Nurgaliev <nurgadison@gmail.com>
2026-03-03 20:28:34 +0300

    Added command parser and modified match/case block

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ class Task:
 def print_tasks(category='all'):
     pass
 
+def parse_description(command):
+    started = 0
+    ended = len(command) - 1
+    if command[ended] != '"':
+        return False
+    for i in range(ended):
+        if command[i] == '"':
+            started = i
+            break
+    else:
+        return False
+    return command[started + 1:ended]
+
 def main():
     while True:
         command = input('Enter command > ')
@@ -19,34 +32,44 @@ def main():
             case ['list']:
                 print('List of all tasks')
                 print_tasks()
-            case 'list', 'done':
-                print('List of all tasks done')
-                print_tasks('done')
-            case 'list', 'todo':
-                print('List of all tasks todo')
-                print_tasks('todo')
-            case 'list', 'in-progress':
-                print('List of all tasks in-progress')
-                print_tasks('in-progress')
+            case 'list', 'done' | 'todo' | 'in-progress' as category:
+                print_tasks(category)
             case 'list', *wtv:
                 print('Incorrect category')
             case ['add']:
                 print('Please enter a task')
             case 'add', *info:
-                task = ' '.join(info)
-                if task[0] == task[-1] == '"':
-                    task = task[1:-1]
+                task = parse_description(command)
+                if task:
                     print (f'Adding task: {task}')
                 else:
-                    print('Please enter a task with "+"')
+                    print('Please enter a task within double quotes')
             case 'update', task_id:
                 if task_id.isdigit():
-                    print(f'Updating task with id: {task_id}')
+                    print(f'Please enter an update')
                 else:
                     print('Invalid id')
+            case 'update', task_id, *info:
+                if not task_id.isdigit():
+                    print('Invalid id')
+                    continue
+                task = parse_description(command)
+                if task:
+                    print (f'Updating task: {task} for id: {task_id}')
+                else:
+                    print('Please enter a task within double quotes')
             case ['update']:
-                print('Please enter task id')
+                print('Please enter task id and then task within double quotes')
             case 'update', *wtv:
+                print('Invalid id')
+            case 'delete', task_id:
+                if task_id.isdigit():
+                    print(f'Deleting task with id: {task_id}')
+                else:
+                    print('Invalid id')
+            case ['delete']:
+                print('Please enter task id')
+            case 'delete', *wtv:
                 print('Invalid id')
 
 if __name__ == '__main__':
